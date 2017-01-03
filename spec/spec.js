67bdc5077ce602f36c054c72ea3b180de893fd21
neuromancer977 <neuromancer977@gmail.com>
2017-01-03 15:30:32 +0100

    update

---
+++ spec/spec.js
@@ -0,0 +1,21 @@
+describe("A suite", function() {
+
+  beforeEach(function(){
+    console.log('beforeEach');
+  });
+
+  before(function(){
+    console.log('before');
+  });
+  after(function(){
+    console.log('after');
+  });
+  afterEach(function(){
+    console.log('afterEach');
+  });
+
+  it("contains spec with an expectation", function() {
+    expect(true).to.be.true;
+    expect(2).to.equal(2);
+  });
+});
